7ae72fe4ae17e24ccd39584ad18a1723d15a55a1
tj710 <tom.james10@imperial.ac.uk>
2022-05-25 19:16:18 +0200

    Revert "Add script to build RPM (second try)"

--- scripts/vivado/reset_scouting-proc.tcl
+++ scripts/vivado/reset_scouting-proc.tcl
@@ -3,9 +3,9 @@ connect_hw_server -url localhost:3121
 current_hw_target [get_hw_targets */xilinx_tcf/Xilinx/1234-tulA]
 set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Xilinx/1234-tulA]
 open_hw_target
-set_property PROGRAM.FILE {/home/scouter/bitfiles/currently_used/scouting_build.bit} [get_hw_devices xcku115_0]
-set_property PROBES.FILE {/home/scouter/bitfiles/currently_used/scouting_build.ltx} [get_hw_devices xcku115_0]
-set_property FULL_PROBES.FILE {/home/scouter/bitfiles/currently_used/scouting_build.ltx} [get_hw_devices xcku115_0]
+set_property PROGRAM.FILE {/home/scouter/bitfiles/eight_links_zs/top.bit} [get_hw_devices xcku115_0]
+set_property PROBES.FILE {/home/scouter/bitfiles/eight_links_zs/top.ltx} [get_hw_devices xcku115_0]
+set_property FULL_PROBES.FILE {/home/scouter/bitfiles/eight_links_zs/top.ltx} [get_hw_devices xcku115_0]
 current_hw_device [get_hw_devices xcku115_0]
 refresh_hw_device [lindex [get_hw_devices xcku115_0] 0]
 
--- src/scdaq.cc
+++ src/scdaq.cc
@@ -105,34 +105,12 @@ int run_pipeline( int nbThreads, ctrl& control, config& conf )
 int main( int argc, char* argv[] ) {
   (void)(argc);
   (void)(argv);
-
-if(argc < 2){
-		LOG(DEBUG) << "no arguments provided to scdaq, try --help";
-		return 1;
-	
-		}	
-
-	if((std::string(argv[1]) == "-h") || (std::string(argv[1]) == "--help")){
-		LOG(DEBUG) << "HELP: expected arguments --config [configfilename]";
-		return 1;
-	}
-
-	if((argc != 3)){
-		LOG(ERROR) << "error occurred, number of arguments != 2, expected --config [configfilename] , try --help";
-		return 1;
-	}
-	
-	if (std::string(argv[1]) == "--config"){
- 		LOG(DEBUG) << "scdaq started with conffile" << std::string(argv[2]);
-		}else{
-		LOG(ERROR) << "invalid argument, expected --config, see --help";
-		return 1;
-	}
+  LOG(DEBUG) << "here 0";
 
   try {
-    config conf(argv[1]);
+    config conf("scdaq.conf");
     conf.print();
-    LOG(DEBUG) << "configuration loaded";
+    LOG(DEBUG) << "here 1";
     ctrl control;
     //    tbb::tick_count mainStartTime = tbb::tick_count::now();
 
